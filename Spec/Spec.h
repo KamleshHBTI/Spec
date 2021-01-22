795aa7c11ad7ed705f78ec75fef54b06fbee5371
Kamlesh Kumar <kamkumar@dminc.com>
2021-01-22 16:16:16 +0530

    Initial Commit

---
+++ Spec/Spec.h
@@ -0,0 +1,18 @@
+//
+//  Spec.h
+//  Spec
+//
+//  Created by Kamlesh on 22/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Spec.
+FOUNDATION_EXPORT double SpecVersionNumber;
+
+//! Project version string for Spec.
+FOUNDATION_EXPORT const unsigned char SpecVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Spec/PublicHeader.h>
+
+
---
+++ Spec/SpecConfig.swift
@@ -0,0 +1,8 @@
+//
+//  SpecConfig.swift
+//  Spec
+//
+//  Created by Kamlesh on 22/01/21.
+//
+
+import Foundation
